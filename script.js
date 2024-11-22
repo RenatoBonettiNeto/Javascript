8ebc4e21819c5201c7b5fb953b26123d26bfcbc2
RenatoBonettiNeto <017bnttinto.renato28sz@gmail.com>
2024-11-21 21:37:28 -0300

    Aprendido sobre Transformar String em número e número inteiro (parseInt) para número com virgula (parseFloat).

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-var a = 10;
-var b = 20;
+var a = "10";
+var b = 20.5;
 
+var c = parseInt(a) + parseFloat(b);
 
-var soma = a + b;
-var subtracao = a - b;
-var multiplicacao = a * b;
-var divisao = a / b;
-var restoDivisao = a % b;
-var incremento = ++a;
-var decremento = --b;
-
-console.log("Soma: ", soma);
-console.log("Subtração: ", subtracao);
-console.log("Multiplicação:", multiplicacao);
-console.log("Divisão: ", divisao);
-console.log("RestoDivisao: ", restoDivisao);
-console.log("Incremento: ", incremento);
-console.log("Decremento: ", decremento);
+console.log(c);
