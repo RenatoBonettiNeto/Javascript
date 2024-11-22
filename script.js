51b99a6b41463d271ccc15584ae1240e88a1b96a
RenatoBonettiNeto <017bnttinto.renato28sz@gmail.com>
2024-11-21 21:16:02 -0300

    Aprendido sobre concatenação.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-var  a;
 
-a = "Esta é minha variavel 'a'";
-var b = "Esta é minha variavel 'b'"
 
-console.log(a,"-",b);
+var a = "Primeira Linha \n Segunda Linha";
+
+var b = 1;
+var c = 1;
+
+
+console.log(a);
+console.log("Soma:", b + c);
+
+//Concatenação:
+console.log ("Concatenação:" + "1" + "1");
