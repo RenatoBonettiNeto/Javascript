b2f0d1c2c4a50e32091f75610ad3ec27f2a0c1bd
RenatoBonettiNeto <017bnttinto.renato28sz@gmail.com>
2024-11-21 21:55:17 -0300

    Aprendido sobre Operador Ternário

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
+var idade = prompt("Qual a sua idade?");
 
-var idade = parseInt(prompt("Digite sua idade: "));
-
-if (idade >= 18){
-    console.log("Você é maior de idade!");
-    console.log("Aproveite!")
-}else{
-    console.log("Você é menor de idade!");
-    console.log("Volte Futuramente!");
-}
+idade >= 18 ? console.log("Você é maior de idade!") : console.log("Você é menor de idade!");
