17131ba28490596360a8eda4d1e7fcd6b70002cd
RenatoBonettiNeto <017bnttinto.renato28sz@gmail.com>
2024-11-21 21:27:04 -0300

    Aprendido sobre operações aritméticas

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+var a = 10;
+var b = 20;
 
 
-var a = "Primeira Linha \n Segunda Linha";
+var soma = a + b;
+var subtracao = a - b;
+var multiplicacao = a * b;
+var divisao = a / b;
+var restoDivisao = a % b;
+var incremento = ++a;
+var decremento = --b;
 
-var b = 1;
-var c = 1;
-
-
-console.log(a);
-console.log("Soma:", b + c);
-
-//Concatenação:
-console.log ("Concatenação:" + "1" + "1");
+console.log("Soma: ", soma);
+console.log("Subtração: ", subtracao);
+console.log("Multiplicação:", multiplicacao);
+console.log("Divisão: ", divisao);
+console.log("RestoDivisao: ", restoDivisao);
+console.log("Incremento: ", incremento);
+console.log("Decremento: ", decremento);
