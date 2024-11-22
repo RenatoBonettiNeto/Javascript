984a19f5374fe43a7418d73bb83f86fa78037424
RenatoBonettiNeto <017bnttinto.renato28sz@gmail.com>
2024-11-21 22:14:05 -0300

    Aperfeiçoamento de Condicionais e aprendizado sobre Switch

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-var idade = prompt("Qual a sua idade?");
+var primeiraNota = parseFloat(prompt("Informe a primeira nota: "));
+var segundaNota = parseFloat(prompt("Informe a segunda nota."));
+var conceito = "";
 
-idade >= 18 ? console.log("Você é maior de idade!") : console.log("Você é menor de idade!");
+var media = (primeiraNota + segundaNota) / 2;
+
+if (primeiraNota > 10 || segundaNota > 10){
+    console.log("Nota não deve ser maior que 10!")
+}
+else if(media >= 8.0){
+    conceito = "Ótimo"
+}
+else if( media >= 6.5){
+    conceito = "Bom"
+}
+else {
+    conceito = "Regular"
+} 
+
+console.log(media);
+console.log(conceito);
+
+switch(conceito){
+
+    case "Ótimo":
+        console.log("Meus parabéns!");
+        break;
+
+    case "Bom":
+        console.log("Você está indo muito bem!");
+        break;
+
+    case "Regular":
+        console.log("Você precisa estudar mais!")
+        break;
+
+    default:
+        console.log("Houve algum erro!");
+        break;
+    }
