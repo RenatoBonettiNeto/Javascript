ab483283e50d8bbe181b974c7dc60ea34fc163dc
RenatoBonettiNeto <017bnttinto.renato28sz@gmail.com>
2024-11-23 10:37:24 -0300

    Aprendido sobre For.

--- script.js
+++ script.js
@@ -1,40 +1,9 @@
-var primeiraNota = parseFloat(prompt("Informe a primeira nota: "));
-var segundaNota = parseFloat(prompt("Informe a segunda nota."));
-var conceito = "";
 
-var media = (primeiraNota + segundaNota) / 2;
+var numero = 5;
 
-if (primeiraNota > 10 || segundaNota > 10){
-    console.log("Nota não deve ser maior que 10!")
+for (var i = 0; i < numero; i ++){
+
+ console.log("Executando o número:", i)
 }
-else if(media >= 8.0){
-    conceito = "Ótimo"
-}
-else if( media >= 6.5){
-    conceito = "Bom"
-}
-else {
-    conceito = "Regular"
-} 
 
-console.log(media);
-console.log(conceito);
-
-switch(conceito){
-
-    case "Ótimo":
-        console.log("Meus parabéns!");
-        break;
-
-    case "Bom":
-        console.log("Você está indo muito bem!");
-        break;
-
-    case "Regular":
-        console.log("Você precisa estudar mais!")
-        break;
-
-    default:
-        console.log("Houve algum erro!");
-        break;
-    }
+ console.log("Comcluido!")
