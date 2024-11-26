1aa05a996c454ed411c4c035e0f67662ae1f808e
RenatoBonettiNeto <017bnttinto.renato28sz@gmail.com>
2024-11-25 21:27:46 -0300

    Aperfeiçoado o aprendizado de Objetos, aprendendo sobre construtores.

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 
-function calcMedia(){
-    return (this.notas[0] + this.notas[1]) / 2;
+// var calcMedia = function(){
+//     return (this.nota1 + this.nota2)/2;
 
+
+// function criarAluno(nome, n1, n2){
+//     return{    
+//         nome: nome,
+//         nota1: n1,
+//         nota2: n2,
+//         media: function(){
+//             return (this.nota1 + this.nota2)/2;
+//         }
+//     }
+// }
+
+function aluno(nome, n1, n2){
+    this.nome = nome;
+    this.nota1 = n1;
+    this.nota2 = n2;
+
+
+this.media = function(){
+    return (this.nota1 + this.nota2) / 2;
+    }
 }
-    
-var aluno = {
-    nome: "Igor",
-    notas: [9,8],
 
-    media: calcMedia
-}
-    
-var aluno1 = {
-    nome: "Igor",
-    notas: [9,8],
 
-    media: calcMedia
+var a = new aluno("Igor", 8, 7);
+var b = new aluno("João",6,7);
 
-}
+console.log(a.media() + " - " +  b.media());
 
-console.log(aluno.nome)
-console.log(aluno.media());
 
-console.log(aluno1.nome)
-console.log(aluno1.media());
 
-calcMedia();
+// var turma = [
+//     criarAluno("Igor", 9, 6),
+//     criarAluno("João", 7, 4)
+//  ]
 
+// var aluno = turma[0];
+
+// turma.forEach(function (elemento){
+//     console.log(elemento)
+// })
+
+// console.log(aluno);
+// console.log(aluno.media());
