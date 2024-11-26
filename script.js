df6aafb0fd0a095139d0fa805b336972d20239db
RenatoBonettiNeto <017bnttinto.renato28sz@gmail.com>
2024-11-25 21:06:17 -0300

    Aprendido sobre Objetos - Métodos

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 
-var aluno= { nome:"Igor",
-                notas: [7.5, 5.0]
+function calcMedia(){
+    return (this.notas[0] + this.notas[1]) / 2;
+
+}
+    
+var aluno = {
+    nome: "Igor",
+    notas: [9,8],
+
+    media: calcMedia
+}
+    
+var aluno1 = {
+    nome: "Igor",
+    notas: [9,8],
+
+    media: calcMedia
+
 }
 
-var novaProp = "Sobrenome";
+console.log(aluno.nome)
+console.log(aluno.media());
 
-aluno.matricula = 12345;
-aluno[novaProp ] = "Oliveira";
+console.log(aluno1.nome)
+console.log(aluno1.media());
 
+calcMedia();
 
-console.log(aluno);
