626dfa9309eed72cfb3a87b6cf97f107044099c1
RenatoBonettiNeto <017bnttinto.renato28sz@gmail.com>
2024-12-03 22:21:22 -0300

    Aprendido sobre datas

--- script.js
+++ script.js
@@ -1,48 +1,7 @@
 
-// var calcMedia = function(){
-//     return (this.nota1 + this.nota2)/2;
+var d = new Date("8-28-2003 00:00");
 
-
-// function criarAluno(nome, n1, n2){
-//     return{    
-//         nome: nome,
-//         nota1: n1,
-//         nota2: n2,
-//         media: function(){
-//             return (this.nota1 + this.nota2)/2;
-//         }
-//     }
-// }
-
-function aluno(nome, n1, n2){
-    this.nome = nome;
-    this.nota1 = n1;
-    this.nota2 = n2;
-
-
-this.media = function(){
-    return (this.nota1 + this.nota2) / 2;
-    }
-}
-
-
-var a = new aluno("Igor", 8, 7);
-var b = new aluno("João",6,7);
-
-console.log(a.media() + " - " +  b.media());
-
-
-
-// var turma = [
-//     criarAluno("Igor", 9, 6),
-//     criarAluno("João", 7, 4)
-//  ]
-
-// var aluno = turma[0];
-
-// turma.forEach(function (elemento){
-//     console.log(elemento)
-// })
-
-// console.log(aluno);
-// console.log(aluno.media());
+console.log(d)
+console.log(d.getDate());
+console.log(d.getMonth()) // 0 - 11
+console.log(d.getFullYear());
